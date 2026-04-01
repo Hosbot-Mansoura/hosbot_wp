002d32e9ad003d3f653182f8d34109c5dbe79827
Yocif-Ref3t <dev.yossifrefaat@gmail.com>
2026-04-01 20:32:46 +0200

    Create hardware config file

---
+++ src/hardware_pkg/config/hardware_config.py
@@ -0,0 +1,12 @@
+########## [ GENERAL ] ##########
+RPI_PIN_MODE = 'BCM'
+
+########## [ MOTORS ] ##########
+LEFT_DIR_PIN_NUM = 17
+LEFT_PWM_PIN_NUM = 18
+RIGHT_DIR_PIN_NUM = 23
+RIGHT_PWM_PIN_NUM = 24
+LEFT_MOTOR_SCALE = 1.0
+RIGHT_MOTOR_SCALE = 1.0
+
+########## [ ENCODERS ] ##########
