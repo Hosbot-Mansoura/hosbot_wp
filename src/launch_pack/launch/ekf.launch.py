44df4f9c440786e891661dc0dc65e4169b49aaf7
Yocif-Ref3t <dev.yossifrefaat@gmail.com>
2026-04-08 23:18:07 +0200

    add imu to ekf launch file

--- src/launch_pack/launch/ekf.launch.py
+++ src/launch_pack/launch/ekf.launch.py
@@ -8,6 +8,8 @@ def generate_launch_description():
     ld = LaunchDescription()
     hardware_package_path = get_package_share_directory('hardware_pkg')
     ekf_config_files = [os.path.join(hardware_package_path,'config','ekf_params.yaml')]
+    imu_config_files = [os.path.join(hardware_package_path,'config','params.yaml')]
+
 
     ekf_node = Node(
         package='robot_localization',
@@ -17,6 +19,14 @@ def generate_launch_description():
         parameters=ekf_config_files
     )
 
-    ld.add_action(ekf_node)
+    imu_node = Node(
+        package='hardware_pkg',
+        exec_name='imu_node',
+        name = "imu_node",
+        parameters=imu_config_files,
+        output = 'screen'
+    )
 
+    ld.add_action(imu_node)
+    ld.add_action(ekf_node)
     return ld
