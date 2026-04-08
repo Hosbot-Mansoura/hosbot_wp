f491f2171689cf74f52228c1416bbada83ff6b50
Mariam Alkhouli <mariamusama4@gmail.com>
2026-04-08 10:01:50 +0200

    remove build , log and install folder

--- build/launch_pack/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hosbot/hosbot_wp/src/launch_pack
-# Build directory: /home/hosbot/hosbot_wp/build/launch_pack
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
