f6cf42e5d71035d031c3d0faeb77a3aa0547789d
Yocif-Ref3t <dev.yossifrefaat@gmail.com>
2026-04-08 23:11:34 +0200

    Create ekf launch file

---
+++ src/launch_pack/launch/ekf.launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import ThisLaunchFileDir
+from ament_index_python.packages import get_package_share_directory
+import os
+
+def generate_launch_description():
+    ld = LaunchDescription()
+    hardware_package_path = get_package_share_directory('hardware_pkg')
+    ekf_config_files = [os.path.join(hardware_package_path,'config','ekf_params.yaml')]
+
+    ekf_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+        parameters=ekf_config_files
+    )
+
+    ld.add_action(ekf_node)
+
+    return ld
