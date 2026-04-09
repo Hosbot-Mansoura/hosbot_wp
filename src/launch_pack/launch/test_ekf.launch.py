b412cb26f8e44f0a00f6d54958e1e6fc5845f121
Yocif-Ref3t <dev.yossifrefaat@gmail.com>
2026-04-09 15:38:38 +0200

    test ekf

---
+++ src/launch_pack/launch/test_ekf.launch.py
@@ -0,0 +1,32 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+
+    ekf_config = os.path.join(
+        os.getenv('HOME'),
+        'your_package/src/your_package/config/ekf_imu.yaml'
+    )
+
+    return LaunchDescription([
+
+        # EKF Node
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[ekf_config]
+        ),
+
+        # Static TF (base_link -> imu_link)
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='imu_tf',
+            arguments=['0', '0', '0', '0', '0', '0',
+                       'base_link', 'imu_link']
+        ),
+
+    ])
