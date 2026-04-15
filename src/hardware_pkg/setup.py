b6b16df12e7224d45699f5a5ae50d5a75db51c93
Yocif-Ref3t <dev.yossifrefaat@gmail.com>
2026-04-15 20:27:20 +0200

    create auto map node

--- src/hardware_pkg/setup.py
+++ src/hardware_pkg/setup.py
@@ -33,7 +33,9 @@ setup(
             "keyboard_controller_node = hardware_pkg.keyboard_controller:main",
             "encoder_node = hardware_pkg.encoder_node:main",
             "calibrate_motors_node = hardware_pkg.calibrate_motors_node:main",
-            "imu_node = hardware_pkg.imu_node:main"
+            "imu_node = hardware_pkg.imu_node:main",
+            "auto_map_node = hardware_pkg.auto_map:main",
+
         ],
     },
 )
