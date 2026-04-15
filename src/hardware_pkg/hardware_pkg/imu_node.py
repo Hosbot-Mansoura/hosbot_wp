7bd0d1fc21a7b92999b43e5dc78ea72096016c84
Yocif-Ref3t <dev.yossifrefaat@gmail.com>
2026-04-15 15:50:54 +0200

    enhance imu node

--- src/hardware_pkg/hardware_pkg/imu_node.py
+++ src/hardware_pkg/hardware_pkg/imu_node.py
@@ -7,6 +7,7 @@
 
 import rclpy
 from rclpy.node import Node
+from collections import deque
 import smbus2
 import time
 import math
@@ -28,9 +29,11 @@ class IMUNode(Node):
         self.i2c_channel = self.get_parameter('I2C_BUS_CHANNEL').value
         self.imu_address = self.get_parameter('IMU_REGISTER_ADDRESS').value
         self.frequency = self.get_parameter('FREQUENCY').value
-        self.gyroscope_scale_factor = 131
+        self.gyroscope_scale_factor = 131.2
+        self.gz_history = deque(maxlen=5)
         self.acceleration_scale_factor = 16384
         self.gravity_value = 9.81
+        self.gyro_deadband = 0.01
         self.gx_bias = 0.0
         self.gy_bias = 0.0
         self.gz_bias = 0.0
@@ -40,7 +43,7 @@ class IMUNode(Node):
         self.init()
         ########## [ CALIBRATE IMU SENSOR ] ##########
         self.get_logger().info('code:imu-2 please keep robot still for calibration')
-        self.calibrate_gyro(samples= 300)
+        self.calibrate_gyro(samples= 400)
         self.get_logger().info("code:imu-3 calibration finished with values")
         self.get_logger().info("gx_bias: " + str(self.gx_bias) + " gy_bias: " + str(self.gy_bias) + " gz_bias: " + str(self.gz_bias) )
 
@@ -111,24 +114,41 @@ class IMUNode(Node):
         # Range [7-12] for acceleration
         ########## [ RAW DATA ] ##########
         imu_msg = Imu()
-        gyro_data = self.bus.read_i2c_block_data(self.imu_address, 0x0c, 6)
-        acc_data = self.bus.read_i2c_block_data(self.imu_address, 0x12, 6)
+        imu_data = self.bus.read_i2c_block_data(self.imu_address, 0x0c, 12)
+    
 
         ########## [ GYROSCOPE ] ##########
-        gx_raw = self.combine(gyro_data[1], gyro_data[0])
-        gy_raw = self.combine(gyro_data[3], gyro_data[2])
-        gz_raw = self.combine(gyro_data[5], gyro_data[4])
+        gx_raw = self.combine(imu_data[1], imu_data[0])
+        gy_raw = self.combine(imu_data[3], imu_data[2])
+        gz_raw = self.combine(imu_data[5], imu_data[4])
 
         # convert to rad/s 
-        gx = self.convert_deg_to_rad(gx_raw / self.gyroscope_scale_factor)
-        gy = self.convert_deg_to_rad(gy_raw / self.gyroscope_scale_factor)
-        gz = self.convert_deg_to_rad(gz_raw / self.gyroscope_scale_factor)
+        gx_rad = self.convert_deg_to_rad(gx_raw / self.gyroscope_scale_factor)
+        gy_rad = self.convert_deg_to_rad(gy_raw / self.gyroscope_scale_factor)
+        gz_rad = self.convert_deg_to_rad(gz_raw / self.gyroscope_scale_factor)
+
+        # apply bias correction
+        gx_rad_bias = gx_rad - self.gx_bias
+        gy_rad_bias = gy_rad - self.gy_bias
+        gz_rad_bias = gz_rad - self.gz_bias
+
+        # smooth gz data using a moving average filter with a window size of 5
+        self.gz_history.append(gz_rad_bias)
+        smoothed_gz = sum(self.gz_history) / len(self.gz_history)
+
+        # apply thresholding to filter out noise (current noise factor = 0.01)
+        gx = 0.0 if abs(gx_rad_bias) < self.gyro_deadband else gx_rad_bias
+        gy = 0.0 if abs(gy_rad_bias) < self.gyro_deadband else gy_rad_bias
+        smoothed_gz = 0.0 if abs(smoothed_gz) < self.gyro_deadband else smoothed_gz
+        
+
         
         ########## [ ACCELERATION ] ##########
-        ax_raw = self.combine(acc_data[1], acc_data[0])
-        ay_raw = self.combine(acc_data[3], acc_data[2])
-        az_raw = self.combine(acc_data[5], acc_data[4])
+        ax_raw = self.combine(imu_data[7], imu_data[6])
+        ay_raw = self.combine(imu_data[9], imu_data[8])
+        az_raw = self.combine(imu_data[11], imu_data[10])
 
+        # convert from raw data to usable data
         ax_in_g = ax_raw / self.acceleration_scale_factor
         ay_in_g = ay_raw / self.acceleration_scale_factor
         az_in_g = az_raw / self.acceleration_scale_factor
@@ -145,10 +165,9 @@ class IMUNode(Node):
         imu_msg.orientation_covariance[4] = 0.0
         imu_msg.orientation_covariance[8] = 0.0
 
-        imu_msg.angular_velocity.x = gx - self.gx_bias
-        imu_msg.angular_velocity.y = gy - self.gy_bias
-        imu_msg.angular_velocity.z = gz - self.gz_bias
-        imu_msg.linear_acceleration.x = ax
+        imu_msg.angular_velocity.x = gx
+        imu_msg.angular_velocity.y = gy 
+        imu_msg.angular_velocity.z = smoothed_gz # IMU main function 
         imu_msg.linear_acceleration.y = ay
         imu_msg.linear_acceleration.z = az
         imu_msg.angular_velocity_covariance = [0.02,0,0,0,0.02,0,0,0,0.02]
